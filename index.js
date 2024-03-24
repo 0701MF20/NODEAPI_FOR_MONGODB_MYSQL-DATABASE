8bef027ca6d9ccc25b61b984c3a449872b559b0e
Mohd Faraz <farazmohd0701@gmail.com>
2024-03-24 15:26:17 +0530

    24march2024

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const router = require("./routes/EmployeeAPI");
 const mongorouter=require("./routes/MongoDBAPI")
 // const mongoRoutes=require("./routes/mongoRoutes")
 // const mongosRouter=require('./routes/mongoRoutes')
-
+// const routermongo=require("./routes/DBAPI")
 app.use(cors());
 app.use(express.json());
 const dbConfig = config.get("db");
@@ -41,5 +41,6 @@ app.use((req,res,next)=>{
 })
 app.use("/crud", router);
 app.use("/mongo", mongorouter);
+// app.use("/check",routermongo)
 // app.use("/mongos",mongosRouter)
 app.listen(3000, () => console.log("listen now."));
